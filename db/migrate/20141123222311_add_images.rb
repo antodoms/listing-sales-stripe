79531aad1452ce56f84db94e74757873f532a2fb
antodoms <antodoms@gmail.com>
2014-11-24 04:13:29 +0530

    removed useless files

--- db/migrate/20141123222311_add_images.rb
+++ db/migrate/20141123222311_add_images.rb
@@ -1,9 +1,9 @@
 class AddImages < ActiveRecord::Migration
   def self.up
-    add_attachment :friends, :avatar
+    add_attachment :listings, :image
   end
 
   def self.down
-    remove_attachment :friends, :avatar
+    remove_attachment :listings, :image
   end
 end
